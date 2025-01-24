7806bc6536b4a603ca7679dc2a3da9c609f0cf5a
lvyshkvarko <96944854+lvyshkvarko@users.noreply.github.com>
2025-01-24 18:55:19 +0300

    Является ли число делителем?
    Определить, является ли число, а делителем числа b или, наоборот, число b делителем числа а. Ответом должны служить сообщения «Да, одно из чисел является делителем другого» или «Нет, ни одно из чисел не является делителем другого».

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите число a: ");
+        int a = int.Parse(Console.ReadLine());
+
+        Console.Write("Введите число b: ");
+        int b = int.Parse(Console.ReadLine());
+
+        if (a % b == 0 || b % a == 0)
+        {
+            Console.WriteLine("Да, одно из чисел является делителем другого");
+        }
+        else
+        {
+            Console.WriteLine("Нет, ни одно из чисел не является делителем другого");
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("Zadacha_ISP-22-4_Vyshkvarko, Dusmekeev")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Zadacha_ISP-22-4_Vyshkvarko, Dusmekeev")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("c584d6af-f2a3-480a-b215-8d445c5e5a3f")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
